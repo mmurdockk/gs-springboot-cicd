db6168ae8a39e645016ee82b116977cb72f669e9
Modzful <adrien.gooris@gmail.com>
2018-01-16 10:08:17 +0100

    Gatling stress test

---
+++ src/test/scala/RecordedSimulation.scala
@@ -0,0 +1,35 @@
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class RecordedSimulation extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://10.221.192.79:8080")
+		.inferHtmlResources()
+		.acceptHeader("image/webp,image/apng,image/*,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("fr")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36")
+
+	val headers_0 = Map(
+		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+		"Upgrade-Insecure-Requests" -> "1")
+
+	val headers_1 = Map("Pragma" -> "no-cache")
+
+
+
+	val scn = scenario("RecordedSimulation")
+		.exec(http("request_0")
+			.get("/greeting")
+			.headers(headers_0)
+			.resources(http("request_1")
+			.get("/favicon.ico")
+			.headers(headers_1)))
+
+	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+}
